8f2f56e904d7b8e558cf056fa66eff897bf7cfd6
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2024-06-29 11:05:35 +0100

    start project

---
+++ src/main/java/com/fabio_trajano/java_financial_analysis/JavaFinancialAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.fabio_trajano.java_financial_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFinancialAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFinancialAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabio_trajano/java_financial_analysis/JavaFinancialAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabio_trajano.java_financial_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFinancialAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
