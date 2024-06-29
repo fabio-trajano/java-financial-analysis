d30dc75cc831aaf9c7d7cd0afb60e711d148bfb9
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2024-06-29 22:11:43 +0100

    add StockDataFetchServiceTest

---
+++ src/test/java/com/fabio_trajano/java_financial_analysis/service/StockDataFetchServiceTest.java
@@ -0,0 +1,48 @@
+package com.fabio_trajano.java_financial_analysis.service;
+
+import com.fabio_trajano.java_financial_analysis.model.TickerRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class StockDataFetchServiceTest {
+
+    @Autowired
+    private StockDataFetchService stockDataFetchService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testPrice_validTicker_returnsPrice() throws Exception {
+        String ticker = "AAPL";
+        TickerRequest tickerRequest = new TickerRequest(ticker);
+
+        Double price = stockDataFetchService.price(tickerRequest);
+        System.out.println("Price: " + price.toString());
+
+        assertNotNull(price, "Price should not be null");
+        assertNotEquals(0.0, "Price should be different from zero");
+    }
+
+    @Test
+    public void testEarningsPerShare_validTicker_returnsEarnings() throws Exception {
+        String ticker = "AAPL";
+        TickerRequest tickerRequest = new TickerRequest(ticker);
+
+        Double earnings = stockDataFetchService.earningsPerShare(tickerRequest);
+        System.out.println("Earnings in the last year: " + earnings.toString());
+
+        assertNotNull(earnings, "Earnings should not be null");
+    }
+
+}
