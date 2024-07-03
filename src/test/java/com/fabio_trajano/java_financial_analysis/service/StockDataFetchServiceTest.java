c77581b2a498d66ddd135711f8505313bd508769
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2024-07-03 20:34:45 +0100

    update tests

--- src/test/java/com/fabio_trajano/java_financial_analysis/service/StockDataFetchServiceTest.java
+++ src/test/java/com/fabio_trajano/java_financial_analysis/service/StockDataFetchServiceTest.java
@@ -1,59 +1,83 @@
 package com.fabio_trajano.java_financial_analysis.service;
 
+import com.fabio_trajano.java_financial_analysis.dto.IncomeResponseDTO;
+import com.fabio_trajano.java_financial_analysis.dto.MetricsDTO;
+import com.fabio_trajano.java_financial_analysis.dto.StockResponseDTO;
 import com.fabio_trajano.java_financial_analysis.model.TickerRequest;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
-
-@ExtendWith(MockitoExtension.class)
 @SpringBootTest
-public class StockDataFetchServiceTest {
+class StockDataFetchServiceTest {
 
     @Autowired
     private StockDataFetchService stockDataFetchService;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private TickerRequest tickerRequest;
 
-    @Test
-    public void testPrice_validTicker_returnsPrice() throws Exception {
-        String ticker = "AAPL";
-        TickerRequest tickerRequest = new TickerRequest(ticker);
-
-        Double price = stockDataFetchService.price(tickerRequest);
-        System.out.println("Price: " + price.toString());
-
-        assertNotNull(price, "Price should not be null");
-        assertNotEquals(0.0, "Price should be different from zero");
+    @BeforeEach
+    void setUp() {
+        tickerRequest = new TickerRequest("AAPL");
     }
 
     @Test
-    public void testEarningsPerShare_validTicker_returnsEarnings() throws Exception {
-        String ticker = "AAPL";
-        TickerRequest tickerRequest = new TickerRequest(ticker);
+    void testStockPrice_validTicker_returnsStockInfo() {
+        StockResponseDTO stockInfo = stockDataFetchService.stockPrice(tickerRequest);
 
+        assertNotNull(stockInfo, "Stock info should not be null");
+        assertEquals("AAPL", stockInfo.symbol(), "Symbol should match the request");
+        assertTrue(stockInfo.price() > 0, "Price should be greater than zero");
+
+        System.out.println("Stock Price: " + stockInfo.price());
+    }
+
+    @Test
+    void testEarningsPerShare_validTicker_returnsEarnings() {
         Double earnings = stockDataFetchService.earningsPerShare(tickerRequest);
-        System.out.println("Earnings in the last year: " + earnings.toString());
 
         assertNotNull(earnings, "Earnings should not be null");
+        assertTrue(earnings != 0, "Earnings should not be zero");
+
+        System.out.println("Earnings in the last year: " + earnings);
     }
 
     @Test
-    public void testDividens_validTicker_returnsDividends() throws Exception {
-        String ticker = "AAPL";
-        TickerRequest tickerRequest = new TickerRequest(ticker);
-
+    void testAnnualDividend_validTicker_returnsDividends() {
         Double dividends = stockDataFetchService.annualDividend(tickerRequest);
-        System.out.println("Dividends in the last year: " + dividends.toString());
 
-        assertNotNull(dividends, "Earnings should not be null");
+        assertNotNull(dividends, "Dividends should not be null");
+        assertTrue(dividends >= 0, "Dividends should be non-negative");
+
+        System.out.println("Dividends in the last year: " + dividends);
     }
 
+    @Test
+    void testAnnualIncome_validTicker_returnsIncomeData() {
+        IncomeResponseDTO incomeResponse = stockDataFetchService.annualIncome(tickerRequest);
+
+        assertNotNull(incomeResponse, "Income response should not be null");
+        assertNotNull(incomeResponse.calendarYear(), "Calendar year should not be null");
+        assertTrue(incomeResponse.revenue() > 0, "Revenue should be greater than zero");
+        assertTrue(incomeResponse.netIncome() != 0, "Net income should not be zero");
+
+        System.out.println("Annual Income - Year: " + incomeResponse.calendarYear() +
+                ", Revenue: " + incomeResponse.revenue() +
+                ", Net Income: " + incomeResponse.netIncome());
+    }
+
+    @Test
+    void testMetricsDTO_validTicker_returnsMetrics() {
+        MetricsDTO metricsDTO = stockDataFetchService.metricsDTO(tickerRequest);
+
+        assertNotNull(metricsDTO, "Metrics DTO should not be null");
+        assertTrue(metricsDTO.revenuePerShare() > 0, "Revenue per share should be greater than zero");
+        assertTrue(metricsDTO.peRatio() > 0, "P/E ratio should be greater than zero");
+
+        System.out.println("Metrics - Revenue per Share: " + metricsDTO.revenuePerShare() +
+                ", P/E Ratio: " + metricsDTO.peRatio());
+    }
 }
